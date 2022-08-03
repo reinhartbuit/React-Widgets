93ac3fd3d0f3bdeb46be1c8328e9be19fd4e8581
reinhartbuit <buit.reinhart@gmail.com>
2022-08-03 07:53:06 +0200

    Build Accordion Functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a JS FE Framework',
+  },
+  {
+    title: 'Why React?',
+    content: 'React is cool',
+  },
+  {
+    title: 'How do you use react?',
+    content: 'You use components',
+  },
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items}></Accordion>
+    </div>
+  );
 };
