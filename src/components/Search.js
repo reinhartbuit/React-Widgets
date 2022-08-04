fec7e30cfa534818b8b1655b7c8f3c4dfc3b4798
reinhartbuit <buit.reinhart@gmail.com>
2022-08-04 08:29:03 +0200

    Dropdown Widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -18,13 +18,32 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
 
-    search();
+      return () => {
+        clearInterval(timeoutId);
+      };
+    }
   }, [term]);
 
   const rederedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            GO
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
